ad1e79583cef29a91a2c2f8da932f108a06e1957
Alex-dev02 <ionut02alex@yahoo.ro>
2023-03-05 14:37:37 +0200

    User model

---
+++ MemoryTiles/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryTiles.Models
+{
+    public class User
+    {
+        public string Name { get; }
+        public string PathToAvatar { get; }
+        public int PlayedGames { get; set; }
+        public int GamesWon { get; set; }
+
+        public User(string name, string pathToAvatar, int playedGames, int gamesWon)
+        {
+            Name = name;
+            PathToAvatar = pathToAvatar;
+            PlayedGames = playedGames;
+            GamesWon = gamesWon;
+        }
+    }
+}
