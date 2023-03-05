8d5473a6559225206eca50b5f1e1c439ad415a87
Alex-dev02 <ionut02alex@yahoo.ro>
2023-03-05 19:11:56 +0200

    outline of SingIn page View

--- MemoryTiles/App.xaml.cs
+++ MemoryTiles/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MemoryTiles.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,10 @@ namespace MemoryTiles
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SignInView signInView = new SignInView();
+            signInView.Show();
+        }
     }
 }
